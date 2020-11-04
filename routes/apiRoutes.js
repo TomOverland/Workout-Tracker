42ae69d91fd3474897632a6de25ee9e1a05ce9bd
Tom Overland <tomoverland03@gmail.com>
2020-11-04 10:42:42 -0600

    created routes for API

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
         });
     });
 
-    // Adds new workout in Workout db
+    // createWorkout() in api.js, Adds new workout in Workout db
     app.post("/api/workouts", async (req, res) => {
         try {
             const response = await db.Workout.create({type: "workout"})
@@ -22,5 +22,36 @@ module.exports = function(app) {
         }
     })
 
+    // addExercise() in api.js
+    app.put("/api/workouts/:id", ({body, params}, res) => {
+        const workoutId = params.id;
+        let savedExercises = [];
 
-}
+        db.Workout.find({_id: workoutId}).then(dbWorkout => {
+            savedExercises = dbWorkout[0].exercises;
+            res.json(dbWorkout[0].exercises);
+            let allExercises = [...savedExercises, body]
+            updateWorkout(allExercises);
+        }).catch(err => {
+            res.json(err)
+        });
+
+    function updateWorkout(exercises){
+        db.Workout.findByIdAndUpdate(workoutId, {exercises: exercises}, function(err, doc){
+            if(err){
+                console.log(err)
+            }
+        })
+    }
+
+    })
+
+    // getWorkoutsInRange() in api.js
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).then(workout => {
+            res.json(workout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+};
