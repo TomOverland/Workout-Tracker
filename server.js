61d7adef79ae5b795ebad75e5d65092b92769c83
Tom Overland <tomoverland03@gmail.com>
2020-11-04 15:14:03 -0600

    added comments for clarity

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
+// Dependencies
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
+// Setting up Express
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
+// Setting up Morgan, which is a logger
 app.use(morgan("dev"));
 
+// Setting up express app to handle data parsing
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-const db = require("./models");
+// Setting up Mongo DB
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Creating Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+// Starts server to begin listening
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}.`)
+    console.log(`App listening on port ${PORT}.`)
 });
